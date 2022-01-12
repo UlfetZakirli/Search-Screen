8b6e7106733671941a32d61d7d4e54736e24560f
UlfetZakirli <88549805+UlfetZakirli@users.noreply.github.com>
2022-01-12 11:02:03 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.querySelector(".search-icon").addEventListener('click', function(){
+    document.querySelector(".search-screen").classList.add("active");
+});
+
+document.querySelector(".close-icon").addEventListener('click',function(){
+    document.querySelector('.search-screen').classList.remove("active");
+});
